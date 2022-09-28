43bcdefa14aaf0e1459966178d4c7fa375b5fbdb
Thiago S. Santos <thiagos.xsantos@gmail.com>
2022-09-27 23:03:20 -0300

    Update controllers and routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :set_article, only: [:create]
+  before_action :set_comment, only: [:destroy]
   http_basic_authenticate_with name: "tss", password: "secret", only: [:destroy]
 
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article), status: :see_other
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
+    def set_comment
+      set_article
+      @comment = @article.comments.find(params[:id])
+    end
+
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "articles#index"
+  root "welcome#index"
 
   resources :articles do
     resources :comments
   end
+
+  get "welcome/index"
 end
